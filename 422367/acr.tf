7e76c0fbe80a3d07984251c72ace0d7dc5d76691
polo910 <mikolaj.polok19@gmail.com>
2026-06-16 19:42:26 +0200

    main

--- 422367/acr.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_container_registry" "acr" {
-  name                = "${var.prefix}acr"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Standard"
-  admin_enabled       = true
-}
-
--- 422367/main.tf
+++ 422367/main.tf
@@ -1,50 +1,19 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "dev422367tfstate"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
-  }
-
+  required_version = ">= 1.5"
+ 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
+      version = "~> 3.110"
     }
   }
 }
-
+ 
 provider "azurerm" {
   features {}
 }
-
-provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
-}
-
+ 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.prefix}-rg"
   location = var.location
 }
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.prefix}-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "aks_subnet" {
-  name                 = "aks-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.0.0/22"]
-}
-# trigger infra pipeline 5.0

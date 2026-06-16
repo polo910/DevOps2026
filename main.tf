28798a7099152350f179de236dca05a016cf9b30
polo910 <mikolaj.polok19@gmail.com>
2026-06-16 15:49:22 +0200

    trigger: run terraform infra pipeline with active session payload

--- main.tf
+++ main.tf
@@ -52,3 +52,4 @@ resource "azurerm_subnet" "aks_subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/22"]
 }
+# force trigger infra pipeline 5.0
